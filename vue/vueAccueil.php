edf433b12c237aca718f84a50c8b578edbdcd671
Ast415 <bastienbories81@gmail.com>
2023-01-22 18:41:18 +0100

    Mise en page de l'accueil

---
+++ vue/vueAccueil.php
@@ -0,0 +1,20 @@
+<main>
+    <div class="mainContentAccueil">
+        <div>
+            <div class="ContentAccueil CAsection1">
+                <div class="photoProfilAccueil"></div>
+            </div>
+            <div class="ContentTypeText ContentAccueil CAsection2">
+                <h1>BORIES Bastien</h1>
+                <p>21 ans et actuellement en deuxième année de BTS SIO (Services Informatiques aux Organisations), option SLAM (Solutions Logicielles et Applications Métiers). J'ai grandi à Castres puis déménagé à Toulouse pour poursuivre mes études.</p>
+                <p>Depuis toujours, Je suis passionné de jeux vidéo et d'informatique, c'est pour cela que j'ai choisi de poursuivre mes études dans ce domaine. Originaire d’un BAC PRO SN j’ai choisie de me diriger vers ce BTS pour pouvoir faire ce qui me plait le plus, de la programmation. Une fois ce BTS fini, je compte faire une formation de développeur web en alternance. Mon objectif pour au moins les 10 prochaine années c’est de devenir développeur fullstack. Mais à long terme, je souhaite devenir développeur de jeux vidéo, ce qui est mon rêve d’enfance.</p>
+                <p>Je suis motivé, travailleur et j'apprécie le travail d'équipe, car je crois que la cohésion d'équipe est importante pour la motivation et l'entraide des personnes.</p>
+            </div>
+        </div>
+        <div class="ContentTypeText contentBtsSio backType1" >
+            <img src="src/img/png/logo-btssio" alt="" class="btsLogo">
+            <p>Le BTS Services informatiques aux Organisations est un diplôme reconnu par l’état de niveau Bac+2. Il remplace, depuis septembre 2011, l’ancien BTS informatique de Gestion (BTS IG). En effet, cette nouvelle formule répond aux attentes de la profession en matière de qualification. Le programme a évolué en fonction de la place et du rôle des TIC (Technologies d’Information Communication) et propose deux spécialités bien distinctes : le BTS SIO SISR et le BTS SIO SLAM.</p>
+            <p>Il associe aux enseignements théoriques et professionnels (en contrôle continu et final) deux stages en entreprise (10 à 11 semaines réparties en première année et en deuxième année d’études), en formation initiale. </p>
+            <p>En plus des matières en lien avec l’ingénierie informatique, la formation diplômante propose également des enseignements de culture générale, d’anglais (avec la possibilité de continuer une LV2), de mathématiques appliquées à l’informatique, ainsi que sur les outils d’analyse économique, managériale et juridique des services informatiques. </p>        
+        </div>
+    </div>
