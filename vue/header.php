6853641dcaab7bad29956a1fea7ddf23d06cfb55
Ast415 <bastienbories81@gmail.com>
2023-01-22 18:39:37 +0100

    Mise en place du menu burger

---
+++ vue/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./src/css/global.css">
+    <link rel="stylesheet" href="./src/css/burger.css">
+</head>
+<body>
+    <header>
+    <nav>
+      <div class="navbar">
+        <div class="container nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>  
+            <div class="menu-items">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">BTS SIO</a></li>
+                <li><a href="#">Projets</a></li>
+                <li><a href="#">Stages</a></li>
+                <li><a href="#">Veilles</a></li>
+                <li><a href="#">Contact</a></li>
+            </div>
+        </div>
+      </div>
+    </nav>
+    </header>
