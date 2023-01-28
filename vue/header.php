3415f7be321200c13c2b357f1be8ed67a324cc5b
Ast415 <bastienbories81@gmail.com>
2023-01-28 17:19:43 +0100

    responsive burger
    quand plein ecran passe de burger a classique

--- vue/header.php
+++ vue/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./src/css/fonts.css">
     <link rel="stylesheet" href="./src/css/global.css">
     <link rel="stylesheet" href="./src/css/burger.css">
 </head>
@@ -15,17 +16,17 @@
         <div class="container nav-container">
             <input class="checkbox" type="checkbox" name="" id="" />
             <div class="hamburger-lines">
-              <span class="line line1"></span>
-              <span class="line line2"></span>
-              <span class="line line3"></span>
+              <span class="line line1 "></span>
+              <span class="line line2 "></span>
+              <span class="line line3 "></span>
             </div>  
             <div class="menu-items">
                 <li><a href="#">Accueil</a></li>
-                <li><a href="#">BTS SIO</a></li>
+                <li><a href="#bts">BTS SIO</a></li>
                 <li><a href="#">Projets</a></li>
                 <li><a href="#">Stages</a></li>
                 <li><a href="#">Veilles</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="#contact">Contact</a></li>
             </div>
         </div>
       </div>
