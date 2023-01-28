6c0419ef3470bf430556476da8b312615eee4f87
Ast415 <bastienbories81@gmail.com>
2023-01-28 17:21:30 +0100

    Mise en page final de l'accueil

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -1,63 +1,76 @@
 <!-- https://coolors.co/131350-0f6de1-dfe5ec-d5759e-1ebff1-8a4282-d12a56 
     https://coolors.co/131350-48392a-f2f3d9-6ccff6-b8336a
 -->
-<main>
-    <div class="homeContent">
-        <div style=" background-color: rgb(19, 19, 80);">
-            <div class="profilContent pCHeader">
-                <div class="profilePhoto shadowStyle1"></div>
-            </div>
-            <div class="profilContent textContent pCText">
-                <h1>BORIES Bastien</h1>
-                <p>21 ans et actuellement en deuxième année de BTS SIO (Services Informatiques aux Organisations), option SLAM (Solutions Logicielles et Applications Métiers). J'ai grandi à Castres puis déménagé à Toulouse pour poursuivre mes études.</p>
-                <p>Depuis toujours, Je suis passionné de jeux vidéo et d'informatique, c'est pour cela que j'ai choisi de poursuivre mes études dans ce domaine. Originaire d’un BAC PRO SN j’ai choisie de me diriger vers ce BTS pour pouvoir faire ce qui me plait le plus, de la programmation. Une fois ce BTS fini, je compte faire une formation de développeur web en alternance. Mon objectif pour au moins les 10 prochaine années c’est de devenir développeur fullstack. Mais à long terme, je souhaite devenir développeur de jeux vidéo, ce qui est mon rêve d’enfance.</p>
-                <p>Je suis motivé, travailleur et j'apprécie le travail d'équipe, car je crois que la cohésion d'équipe est importante pour la motivation et l'entraide des personnes.</p>
-            </div>
-            <div class="borderfC bfCTop btsLogoContent">
-                <img id="bts" src="src/img/png/logo-btssio.png" alt="" class="btsLogo">
-            </div>
-        </div>
-        <div class="formationContent textContent backType1" >
-            <p>Le BTS Services informatiques aux Organisations est un diplôme reconnu par l’état de niveau Bac+2. Il remplace, depuis septembre 2011, l’ancien BTS informatique de Gestion (BTS IG). En effet, cette nouvelle formule répond aux attentes de la profession en matière de qualification. Le programme a évolué en fonction de la place et du rôle des TIC (Technologies d’Information Communication) et propose deux spécialités bien distinctes : le BTS SIO SISR et le BTS SIO SLAM.</p>
-            <p>Il associe aux enseignements théoriques et professionnels (en contrôle continu et final) deux stages en entreprise (10 à 11 semaines réparties en première année et en deuxième année d’études), en formation initiale. </p>
-            <p>En plus des matières en lien avec l’ingénierie informatique, la formation diplômante propose également des enseignements de culture générale, d’anglais (avec la possibilité de continuer une LV2), de mathématiques appliquées à l’informatique, ainsi que sur les outils d’analyse économique, managériale et juridique des services informatiques. </p>        
-        </div>
-        <div class="contactContent">
-            <!-- <div class="demiCercleBande">
-                <div class="demiCercle DCleft"></div>
-                <div class="demiCercle DCright"></div>
-            </div> -->
-            <div class="borderfC bfCBottom shadowStyle1"></div>
-            <div class="textContent">
-                <h1>Contacte</h1>
-                <form action="" method="post">
-                    <div>
-                        Email
-                        <input type="email" name="" id="">
+    <main>
+        <div class="homeContent">
+            <div>
+                <div class="fullProfilContent">
+                    <div class="profilContent pCHeader">
+                        <div class="profilePhoto shadowStyle1"></div>
                     </div>
-                    <div>
-                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <div class="profilContent textContent pCText">
+                        <h1>BORIES Bastien</h1>
+                        <p>21 ans et actuellement en deuxième année de BTS SIO (Services Informatiques aux Organisations), option SLAM (Solutions Logicielles et Applications Métiers). J'ai grandi à Castres puis déménagé à Toulouse pour poursuivre mes études.</p>
+                        <p>Depuis toujours, Je suis passionné de jeux vidéo et d'informatique, c'est pour cela que j'ai choisi de poursuivre mes études dans ce domaine. Originaire d’un BAC PRO SN j’ai choisie de me diriger vers ce BTS pour pouvoir faire ce qui me plait le plus, de la programmation. Une fois ce BTS fini, je compte faire une formation de développeur web en alternance. Mon objectif pour au moins les 10 prochaine années c’est de devenir développeur fullstack. Mais à long terme, je souhaite devenir développeur de jeux vidéo, ce qui est mon rêve d’enfance.</p>
+                        <p>Je suis motivé, travailleur et j'apprécie le travail d'équipe, car je crois que la cohésion d'équipe est importante pour la motivation et l'entraide des personnes.</p>
                     </div>
-                </form>
+                </div>
+                <div id="bts" class="borderfC fCWidth bfCTop btsLogoContent">
+                    <img  src="src/img/png/logo-btssio.png" alt="" class="btsLogo">
+                </div>
             </div>
-            
+            <div class="formationContent textContent fCWidth" >
+                <p>Le BTS Services informatiques aux Organisations est un diplôme reconnu par l’état de niveau Bac+2. Il remplace, depuis septembre 2011, l’ancien BTS informatique de Gestion (BTS IG). En effet, cette nouvelle formule répond aux attentes de la profession en matière de qualification. Le programme a évolué en fonction de la place et du rôle des TIC (Technologies d’Information Communication) et propose deux spécialités bien distinctes : le BTS SIO SISR et le BTS SIO SLAM.</p>
+                <p>Il associe aux enseignements théoriques et professionnels (en contrôle continu et final) deux stages en entreprise (10 à 11 semaines réparties en première année et en deuxième année d’études), en formation initiale. </p>
+                <p>En plus des matières en lien avec l’ingénierie informatique, la formation diplômante propose également des enseignements de culture générale, d’anglais (avec la possibilité de continuer une LV2), de mathématiques appliquées à l’informatique, ainsi que sur les outils d’analyse économique, managériale et juridique des services informatiques. </p>        
+            </div>
+            <div>
+                <!-- <div class="demiCercleBande">
+                    <div class="demiCercle DCleft"></div>
+                    <div class="demiCercle DCright"></div>
+                </div> -->
+                <div class="borderfC fCWidth bfCBottom shadowStyle1"></div>
+                <div class="fullContactContent textContent" id="IDcontact">
+                    <h1 >CONTACT</h1>
+                    <div class="contactContent">
+                        <div class="contactContantText">
+                            <h2>drop message</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, praesentium deleniti itaque recusandae obcaecati nostrum veritatis saepe ipsam ea rerum quam dolor repudiandae eius sint placeat amet laudantium. Quas, deserunt!</p>
+                            <div class="contactContentInfo">
+                                <div class="contactInfo">
+                                    <div class="Qbert reverseBackColor"><img class="icon2" src="src/img/png/telephone1.png" alt=""></div>
+                                    <p>07 81 81 81 81</p>
+                                </div>
+                                <div class="contactInfo">
+                                    <div class="Qbert reverseBackColor"><img class="icon2" src="src/img/png/email1.png" alt=""></div>
+                                    <p>bastienbories81pro@gmail.com</p>
+                                </div>
+                                <div class="contactInfo">
+                                    <div class="Qbert reverseBackColor"><img class="icon2" src="src/img/png/drapeaux1.png" alt=""></div>
+                                    <p>118 Rte de Narbonne, Tripode A<br>31400 Toulouse</p>
+                                </div> 
+                            </div>
+                            
+                        </div>
+                        <div class="contactContantCard">
+                            <form action="#contact" method="post" class="contactCard reverseBackColor" >
+                                <div class="cCInput">
+                                    <input type="text" name="" id="" placeholder="Nom">
+                                </div>
+                                <div class="cCInput">
+                                    <input type="email" name="" id="" placeholder="Email">
+                                </div>
+                                <div class="cCInput ccItextA">
+                                    <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                                </div>
+                                <div class="cCInput">
+                                    <button type="submit" onclick="alert('Fonctionnalité non disponible')">Envoyer</button>
+                                </div>
+                            </form>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    </main>
