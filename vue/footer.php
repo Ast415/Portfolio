69742cb69bd575839b0c7da2c8b24d854c756239
Ast415 <bastienbories81@gmail.com>
2023-01-28 17:21:40 +0100

    proto footer

--- vue/footer.php
+++ vue/footer.php
@@ -1,5 +1,17 @@
 </body>
 <footer>
+    <div class="footInInco">
+
+    </div>
+    <div class="droitAuteurContent">
+        2023 - BORIES Bastien
+    </div>
+    <div class="reseauContent">
+        <div class="Qbert reverseBackColor"></div>
+        <div class="Qbert reverseBackColor"></div>
+        <div class="Qbert reverseBackColor"></div>
+        <div class="Qbert reverseBackColor"></div>
+    </div>
     
 </footer>
 </html>
