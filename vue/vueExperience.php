5e3c82898b377a0414686841a70a5b384c0795e3
Ast415 <bastienbories81@gmail.com>
2023-01-30 20:46:18 +0100

    Création de la page expérience

---
+++ vue/vueExperience.php
@@ -0,0 +1,86 @@
+    <main>
+        <div class="experiencesContent" id="projets">
+            <div class="aExpContent projetsContent">
+                <h1>Mes Projets</h1>
+                <div class="aProjetContent pcContentYear1">
+                    <h2>Projet de 1ere année</h2>
+                    <div class="cardContent">
+                        <div class="Card shadowStyle1">
+                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <h3>GSB</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <h4><a href="">En savoir +</a></h4>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="aProjetContent pcContentYear2">
+                    <h2>Projet de 2eme année</h2>
+                    <div class="cardContent">
+                        <div class="Card shadowStyle1">
+                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <h3>CROS de Lorraine</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <h4><a href="">En savoir +</a></h4>
+                        </div>
+                        <div class="Card shadowStyle1">
+                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <h3>Application Java</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <h4><a href="">En savoir +</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="aProjetContent pcContentPerso">
+                    <h2>Projet personnel</h2>
+                    <div class="cardContent">
+                        <div class="Card shadowStyle1">
+                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <h3>CV / Portfolio Web</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <h4><a href="">En savoir +</a></h4>
+                        </div>
+                        <div class="Card shadowStyle1">
+                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <h3>Serveur Web</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati corporis aliquam consectetur accusantium a cumque, reiciendis sit perspiciatis repudiandae praesentium nihil officia perferendis fugiat, ullam vel magni officiis animi incidunt?</p>
+                            <h4><a href="">En savoir +</a></h4>
+                        </div>
+                        <div class="Card shadowStyle1">
+                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <h3>Lorem</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati corporis aliquam consectetur accusantium a cumque, reiciendis sit perspiciatis repudiandae praesentium nihil officia perferendis fugiat, ullam vel magni officiis animi incidunt?</p>
+                            <h4><a href="">En savoir +</a></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="aExpContent" id="stages">
+                <h1>Mes Stages</h1>
+                <div class="stagesContent">
+                    <div class="aStageContent scContentYear1">
+                        <h2>Stage 1ere année</h2>
+                        <div class="cardContent">
+                            <div class="Card shadowStyle1">
+                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                                <h3>Centre Hospitalier de Lavaur</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                                <h4><a href="">En savoir +</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="aStageContent scContentYear2">
+                        <h2>Stage 2eme année</h2>
+                        <div class="cardContent">
+                            <div class="Card shadowStyle1">
+                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                                <h3>CGX System</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                                <h4><a href="">En savoir +</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
