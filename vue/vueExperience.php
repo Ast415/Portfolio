ad98dded6543979e1493b1b6673670415f21298f
Ast415 <94696044+Ast415@users.noreply.github.com>
2023-01-31 14:01:01 +0100

    Reintégration des image

--- vue/vueExperience.php
+++ vue/vueExperience.php
@@ -6,7 +6,17 @@
                     <h2>Projet de 1ere année</h2>
                     <div class="cardContent">
                         <div class="Card shadowStyle1">
-                            <img class="imgCard" src="src/img/png/gsb.png" alt="">
+                            <div class="imgContent">
+                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            </div>
+                            <h3>GLPI</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <h4><a href="#popup1">En savoir +</a></h4>
+                        </div>
+                        <div class="Card shadowStyle1">
+                            <div class="imgContent">
+                                <img class="imgCard" src="src/img/png/gsb.png" alt="">
+                            </div>
                             <h3>GSB</h3>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
                             <h4><a href="">En savoir +</a></h4>
@@ -17,14 +27,26 @@
                     <h2>Projet de 2eme année</h2>
                     <div class="cardContent">
                         <div class="Card shadowStyle1">
-                            <img class="imgCard" src="src/img/png/logo_CROSL_2022_txtOnly.png" alt="">
-                            <h3>CROS de Lorraine</h3>
+                            <div class="imgContent">
+                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            </div>
+                            <h3>Refonte application PHP</h3>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
                             <h4><a href="">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
-                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <div class="imgContent">
+                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            </div>
                             <h3>Application Java</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <h4><a href="">En savoir +</a></h4>
+                        </div>
+                        <div class="Card shadowStyle1">
+                            <div class="imgContent">
+                                <img class="imgCard2" src="src/img/png/logo_CROSL_2022_txtOnly.png" alt="">
+                            </div>
+                            <h3>CROS de Lorraine</h3>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
                             <h4><a href="">En savoir +</a></h4>
                         </div>
@@ -34,19 +56,25 @@
                     <h2>Projet personnel</h2>
                     <div class="cardContent">
                         <div class="Card shadowStyle1">
-                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <div class="imgContent">
+                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            </div>
                             <h3>CV / Portfolio Web</h3>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
                             <h4><a href="">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
-                            <img class="imgCard" src="src/img/png/raspLogo.png" alt="">
+                            <div class="imgContent">
+                                <img class="imgCard" src="src/img/png/raspLogo.png" alt="">
+                            </div>
                             <h3>Serveur Web</h3>
                             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati corporis aliquam consectetur accusantium a cumque, reiciendis sit perspiciatis repudiandae praesentium nihil officia perferendis fugiat, ullam vel magni officiis animi incidunt?</p>
                             <h4><a href="">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
-                            <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            <div class="imgContent">
+                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                            </div>
                             <h3>Lorem</h3>
                             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati corporis aliquam consectetur accusantium a cumque, reiciendis sit perspiciatis repudiandae praesentium nihil officia perferendis fugiat, ullam vel magni officiis animi incidunt?</p>
                             <h4><a href="">En savoir +</a></h4>
@@ -74,7 +102,9 @@
                         <h2>Stage 2eme année</h2>
                         <div class="cardContent">
                             <div class="Card shadowStyle1">
-                                <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
+                                <div class="imgContent">
+                                    <img class="imgCard2" src="src/img/png/cropped-LOGO-CGX-SYSTEM-V2-BLEU-CIEL-DEFINITIF.png" alt="">
+                                </div>
                                 <h3>CGX System</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
                                 <h4><a href="">En savoir +</a></h4>
@@ -83,5 +113,6 @@
                     </div>
                 </div>
             </div>
+            <?php include ("vuePopup.php"); ?>
         </div>
     </main>
