41248acc55106f2246639c095dba2cbe94200ed8
Ast415 <bastienbories81@gmail.com>
2023-01-31 21:45:11 +0100

    Update vuePopup.php

--- vue/vuePopup.php
+++ vue/vuePopup.php
@@ -1,9 +1,29 @@
-<div id="popup1" class="overlay">
+<div id="default" class="overlay">
     <div class="popup">
-        <h2>Here i am</h2>
+        <h2>Oupss ...</h2>
         <a class="close" href="#">&times;</a>
         <div class="content">
-            Thank to pop me out of that button, but now i'm done so you can close this window.
+            <div class="imgContentPop">
+                <img class="imgPop" src="src/img/png/french-1200px-bsodwindows10.png" alt="">
+            </div>
+            <p>Désolé, la page que vous cherchez n'a pas été trouvée. Il est possible que le lien soit rompu ou que la page ait été supprimée. Veuillez vérifier l'adresse URL ou utiliser la barre de recherche pour trouver ce que vous cherchez. Nous nous excusons pour tout désagrément causé.</p>
+            <p style="margin-bottom: 50vh;" >Si le problème persiste <a href="./?action=accueil#contact">contactez-moi</a> !</p>
+            <div style="width: 100%; display: flex; justify-content: center">
+                <img style="border-radius: 10px; max-width: 474px; width: 100%;" src="src/img/gif/david-goodenough-joueur-du-grenier.gif" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<div id="serveur" class="overlay">
+    <div class="popup">
+        <h2>Serveur Web</h2>
+        <a class="close" href="#">&times;</a>
+        <div class="content">
+            <div class="imgContentPop">
+                <img class="imgPop" src="src/img/jpg/293739.jpg" alt="">
+            </div>
+            <p>J'ai effectué un projet d'installation et configuration d'un serveur web sur un Raspberry pi 4 il y a plus de 6 mois. J'ai tout d'abord installé le système d'exploitation Raspbian sur la carte SD du Raspberry pi.</p>
+            <p>Ensuite, j'ai configuré les paramètres réseau pour accéder à Internet et j'ai installé Apache, le serveur web le plus couramment utilisé. J'ai également installé PHP et MySQL pour permettre l'exécution de scripts PHP sur le serveur. Enfin, j'ai testé le serveur en publiant un site Web simple et j'ai vérifié que tout fonctionnait correctement en accédant à ce site à partir d'un ordinateur distant. Je suis satisfait du résultat final et je suis fier d'avoir accompli ce projet avec succès.</p>
         </div>
     </div>
 </div>
