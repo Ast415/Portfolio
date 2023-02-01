7c154b90855cd798ee6aac26908c3683ad38452a
Ast415 <94696044+Ast415@users.noreply.github.com>
2023-02-01 10:49:36 +0100

    Redaction d'activité

--- vue/vueExperience.php
+++ vue/vueExperience.php
@@ -10,16 +10,20 @@
                                 <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
                             </div>
                             <h3>GLPI</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
-                            <h4><a href="#default">En savoir +</a></h4>
+                            <div class="textCardContent">
+                                <p>Par groupe de deux, ce travail consistait en une étude de découverte de l'outil GLPI (Gestion Libre de Parc Informatique) en vue d'une utilisation pour la gestion du parc informatique d'un lycée.</p>
+                            </div>
+                            <h4><a href="#glpi">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
                             <div class="imgContent">
                                 <img class="imgCard" src="src/img/png/gsb.png" alt="">
                             </div>
-                            <h3>GSB</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
-                            <h4><a href="#default">En savoir +</a></h4>
+                            <h3>GSB Projet APPLI-RAPPORTS</h3>
+                            <div class="textCardContent">
+                                <p>L'objectif était de concevoir une application web pour les visiteurs médicaux qui souhaitaient pouvoir présenter des produits, suivre les remplaçants et améliorer l'efficacité des comptes-rendus.</p>
+                            </div>
+                            <h4><a href="#gsb">En savoir +</a></h4>
                         </div>
                     </div>
                 </div>
@@ -31,7 +35,9 @@
                                 <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
                             </div>
                             <h3>Refonte application PHP</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <div class="textCardContent">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                            </div>
                             <h4><a href="#default">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
@@ -39,7 +45,9 @@
                                 <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
                             </div>
                             <h3>Application Java</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <div class="textCardContent">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                            </div>
                             <h4><a href="#default">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
@@ -47,7 +55,9 @@
                                 <img class="imgCard2" src="src/img/png/logo_CROSL_2022_txtOnly.png" alt="">
                             </div>
                             <h3>CROS de Lorraine</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <div class="textCardContent">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                            </div>
                             <h4><a href="#default">En savoir +</a></h4>
                         </div>
                     </div>
@@ -60,7 +70,9 @@
                                 <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
                             </div>
                             <h3>CV / Portfolio Web</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                            <div class="textCardContent">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                            </div>
                             <h4><a href="#default">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
@@ -68,7 +80,9 @@
                                 <img class="imgCard" src="src/img/png/raspLogo.png" alt="">
                             </div>
                             <h3>Serveur Web</h3>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati corporis aliquam consectetur accusantium a cumque, reiciendis sit perspiciatis repudiandae praesentium nihil officia perferendis fugiat, ullam vel magni officiis animi incidunt?</p>
+                            <div class="textCardContent">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                            </div>
                             <h4><a href="#serveur">En savoir +</a></h4>
                         </div>
                         <div class="Card shadowStyle1">
@@ -76,7 +90,9 @@
                                 <img class="imgCard" src="src/img/png/sora-sagano-3BMIntVUsjQ-unsplash.png" alt="">
                             </div>
                             <h3>Lorem</h3>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati corporis aliquam consectetur accusantium a cumque, reiciendis sit perspiciatis repudiandae praesentium nihil officia perferendis fugiat, ullam vel magni officiis animi incidunt?</p>
+                            <div class="textCardContent">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                            </div>
                             <h4><a href="#default">En savoir +</a></h4>
                         </div>
                     </div>
@@ -93,7 +109,9 @@
                                     <img class="imgCard" src="src/img/png/apple-touch-icon.png" alt="">
                                 </div>
                                 <h3>Centre Hospitalier de Lavaur</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                                 class="textCardContent"<div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                                </div>
                                 <h4><a href="#default">En savoir +</a></h4>
                             </div>
                         </div>
@@ -106,7 +124,9 @@
                                     <img class="imgCard2" src="src/img/png/cropped-LOGO-CGX-SYSTEM-V2-BLEU-CIEL-DEFINITIF.png" alt="">
                                 </div>
                                 <h3>CGX System</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quam ducimus architecto aliquid pariatur aliquam? Ad aliquid saepe maiores praesentium nobis, veritatis iste doloribus quibusdam atque non sequi commodi repudiandae.</p>
+                                 class="textCardContent"<div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id autem ea similique quisquam eos quod, sequi perspiciatis rerum dicta culpa eveniet aliquid sit rem nulla magnam quo, optio voluptatem recusandae.</p>
+                                </div>
                                 <h4><a href="#default">En savoir +</a></h4>
                             </div>
                         </div>
--- vue/vuePopup.php
+++ vue/vuePopup.php
@@ -27,3 +27,40 @@
         </div>
     </div>
 </div>
+<div id="glpi" class="overlay">
+    <div class="popup">
+        <h2>GLPI</h2>
+        <a class="close" href="#">&times;</a>
+        <div class="content">
+            <div class="imgContentPop">
+                <img class="imgPop" src="src/img/png/assets-2.png" alt="">
+            </div>
+            <p>GLPI (Gestionnaire Libre de Parc Informatique) est un logiciel de gestion de parc informatique open-source. Il permet de centraliser l'ensemble des informations relatives à l'infrastructure informatique d'une organisation, telles que les équipements, les licences, les utilisateurs, les tickets d'assistance, les changements, etc.</p>
+            <p>Il propose une interface web conviviale pour la gestion de l'inventaire informatique, la planification des changements et la gestion des incidents. Il offre également des fonctionnalités de reporting pour permettre aux responsables de la gestion du parc informatique de suivre les statistiques et les tendances de leur environnement.</p>
+            <h3>L'objectif du travail</h3>
+            <p>L'objectif de ce travail était de réaliser une étude appliquée de découverte d'un outil de gestion de parc, en particulier de ses fonctionnalités de gestion d'inventaire et de gestion des incidents et demandes (déclaration et suivi). Les étudiants devaient découvrir l'outil GLPI (Gestion Libre de Parc Informatique) et rendre compte de ses fonctionnalités essentielles en alternant entre les rôles d'utilisateur (visiteur médical à terme) et de technicien. Les travaux devaient être réalisés en respectant une succession d'étapes appelées jalons.</p>
+        </div>
+    </div>
+</div>
+<div id="gsb" class="overlay">
+    <div class="popup">
+        <h2>GSB Projet APPLI-RAPPORTS</h2>
+        <a class="close" href="#">&times;</a>
+        <div class="content">
+            <div class="imgContentPop">
+                <img class="imgPop" src="src/img/png/assets-2.png" alt="">
+            </div>
+            <p>Lors de chaque visite chez un médecin, deux produits maximum sont présentés en détail avec des petits rappels sur les autres. Les échantillons offerts ne sont pas forcément liés à ce qui a été présenté. Le nombre d'échantillons distribués doit être enregistré pour des raisons de comptabilité et de surveillance, mais cela est considéré comme pénible car il n'y a pas de moyen de simplement cocher les produits pour lesquels des échantillons ont été donnés.</p>
+            <p>Il peut arriver que l'on tombe sur un remplaçant lors d'une visite chez un médecin et cela peut nécessiter une nouvelle visite. Les praticiens notent souvent les coordonnées des remplaçants qu'ils voient sur les fiches de visite, mais ils aimeraient pouvoir conserver la personne visitée en tant que remplaçant dans le logiciel.</p>
+            <h3>L'objectif du travail</h3>
+            <p> le but de cette mission était de concevoir une application web avec un script côté serveur et une gestion de base de données, tout en répondant aux attentes des clients.</p>
+            <p>Les requêtes les plus importantes à mettre en place</p>
+            <ul>
+                <li>Présentation efficace et mémorisable des produits : les visiteurs souhaitent pouvoir présenter deux produits maximum par visite de manière détaillée, tout en pouvant rappeler rapidement le reste des produits.</li>
+                <li>Souplesse dans la distribution des échantillons : les clients aimeraient avoir plus de liberté dans la distribution des échantillons, sans avoir à enregistrer chaque produit de manière fastidieuse.</li>
+                <li>Suivi des remplaçants : les visiteurs souhaitent pouvoir suivre les remplaçants, même si ceux-ci ne sont pas les médecins permanents du cabinet, en conservant des informations sur les personnes visitées dans leur logiciel.</li>
+                <li>Amélioration de l'efficacité des comptes-rendus : les clients souhaitent pouvoir conserver plus facilement les informations sur les personnes visitées et les produits présentés, sans perdre de temps à saisir des informations inutiles.</li>
+            </ul>
+        </div>
+    </div>
+</div>
