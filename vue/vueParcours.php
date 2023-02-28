cd9157a552fe1e953aa5e640dd0dd6c5ef147bf3
Ast415 <bastienbories81@gmail.com>
2023-02-28 19:08:08 +0100

    Update vueParcours.php

--- vue/vueParcours.php
+++ vue/vueParcours.php
@@ -1,60 +1,60 @@
   <main>
     <div class="timeline">
       <div class="timeline-container left">
-        <div class="date">15 Dec</div>
+        <div class="date">Juil 2017</div>
         <i class="icon fa fa-home"></i>
         <div class="content">
-          <h2>Lorem ipsum dolor sit amet</h2>
+          <h2>Obtention de mon Brevet</h2>
           <p>
             Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
           </p>
         </div>
       </div>
       <div class="timeline-container right">
-        <div class="date">22 Oct</div>
+        <div class="date">Sept 2017</div>
         <i class="icon fa fa-gift"></i>
         <div class="content">
-          <h2>Lorem ipsum dolor sit amet</h2>
+          <h2>Ma seconde générale</h2>
           <p>
             Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
           </p>
         </div>
       </div>
       <div class="timeline-container left">
-        <div class="date">10 Jul</div>
+        <div class="date">Sept 2018</div>
         <i class="icon fa fa-user"></i>
         <div class="content">
-          <h2>Lorem ipsum dolor sit amet</h2>
+          <h2>Réoriantation vers un BAC Pro SN</h2>
           <p>
             Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
           </p>
         </div>
       </div>
       <div class="timeline-container right">
-        <div class="date">18 May</div>
+        <div class="date">Juil 2021</div>
         <i class="icon fa fa-running"></i>
         <div class="content">
-          <h2>Lorem ipsum dolor sit amet</h2>
+          <h2>Obtention de mon BAC Pro</h2>
           <p>
             Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
           </p>
         </div>
       </div>
       <div class="timeline-container left">
-        <div class="date">10 Feb</div>
+        <div class="date">Sept 2021</div>
         <i class="icon fa fa-cog"></i>
         <div class="content">
-          <h2>Lorem ipsum dolor sit amet</h2>
+          <h2>Direction Toulouse pour le BTS SIO</h2>
           <p>
             Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
           </p>
         </div>
       </div>
       <div class="timeline-container right">
-        <div class="date">01 Jan</div>
+        <div class="date">2023 +</div>
         <i class="icon fa fa-certificate"></i>
         <div class="content">
-          <h2>Lorem ipsum dolor sit amet</h2>
+          <h2>A venir ...</h2>
           <p>
             Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
           </p>
