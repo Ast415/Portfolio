93205e110e24785f8d4d80b67a0329542318eea9
Ast415 <bastienbories81@gmail.com>
2023-04-13 11:51:39 +0200

    liens

--- vue/footer.php
+++ vue/footer.php
@@ -2,6 +2,7 @@
 <footer>
     <div class="reseauContent">
         <!-- https://iconmonstr.com/ -->
+        <a href="#top"><div class="icon footer top"></div></a>
         <a href="https://github.com/Ast415" target="_blank"><div class="icon footer git"></div></a>
         <a href="https://www.linkedin.com/in/bastien-bories-a14b8b257/" target="_blank"><div class="icon footer linkedin"></div></a>
         <a href="mailto:bastienbories81pro@gmail.com"><div class="icon footer gmail"></div></a>
@@ -9,8 +10,8 @@
         <a href="#top"><div class="icon footer top"></div></a>
     </div>
     <div class="linkContent">
-        <div>Source</div>
-        <div>Politique de confidentialité</div>
+        <div><a href="">Source</a></div>
+        <div><a href="">Politique de confidentialité</a></div>
     </div>
     <div class="droitAuteurContent">
         © <?php echo date('Y'); ?> Bastien BORIES
