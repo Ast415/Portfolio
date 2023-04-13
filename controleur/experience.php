94158a437b159b547efd9d4a4935bb9ec368bb8d
Ast415 <bastienbories81@gmail.com>
2023-04-13 11:52:15 +0200

    en tete page

--- controleur/experience.php
+++ controleur/experience.php
@@ -3,7 +3,7 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
 
-$titre = "Liste des formation";
+$titre = "Experience";
 include "$racine/vue/header.php";
 include "$racine/vue/vueExperience.php";
 include "$racine/vue/footer.php";
--- controleur/parcours.php
+++ controleur/parcours.php
@@ -3,7 +3,7 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
 
-$titre = "Liste des formation";
+$titre = "Parcours";
 include "$racine/vue/header.php";
 include "$racine/vue/vueParcours.php";
 include "$racine/vue/footer.php";
