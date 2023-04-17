dc1ddcd4389ba7358818100f9b44caf2058006fa
Ast415 <bastienbories81@gmail.com>
2023-04-17 21:32:25 +0200

    Update vueAccueil.php

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -59,7 +59,7 @@
                             </div>
                         </div>
                         <div class="contactContantCard">
-                            <form method="post" class="contactCard reverseBackColor" >
+                            <form method="POST" class="contactCard reverseBackColor" >
                                 <div class="cCInput">
                                     <input type="text" name="name" id="" placeholder="Nom Prenom">
                                 </div>
